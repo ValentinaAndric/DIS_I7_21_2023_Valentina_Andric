708317afb58e862530ea99d7f834cab6e92e117c
Valentina Andric <valentina.andric@united.cloud>
2025-11-18 17:38:20 +0100

    add docker file

--- reservation-service/src/main/java/dis/reservation/reservation_service/controller/ReservationController.java
+++ reservation-service/src/main/java/dis/reservation/reservation_service/controller/ReservationController.java
@@ -1,10 +1,14 @@
 package dis.reservation.reservation_service.controller;
+
 import dis.reservation.reservation_service.dto.CreateReservationRequestDto;
 import dis.reservation.reservation_service.entity.ReservationEntity;
 import dis.reservation.reservation_service.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/reservations")
@@ -14,8 +18,9 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @PostMapping
-    public ResponseEntity<Long> create(@RequestBody CreateReservationRequestDto request) {
+    public ResponseEntity<ReservationEntity> create(@RequestBody CreateReservationRequestDto request) {
+
         ReservationEntity reservationEntity = reservationService.createReservation(request);
-        return ResponseEntity.status(201).body(reservationEntity.getId());
+        return ResponseEntity.status(201).body(reservationEntity);
     }
 }
